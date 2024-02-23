278e92d38c8b3c6ffc2a206e4e80235c68ca2a45
AliDzaki <alidzakifikri@gmail.com>
2024-02-23 20:57:12 +0700

    Menambah file dan halaman untuk home dan membuat sebuah card dan memodifikasi file template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -11,30 +11,25 @@
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom py-3 px-3">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+            <div class="collapse navbar-collapse " id="navbarNav">
+                <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
+    <div class="main" style="min-height: 70vh">
+        @yield('content')
+    </div>
 
     <footer class="bg-light text-dark px-5 py-5 ">
         <div class="container">
